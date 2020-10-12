65ba71f9c5063f6c36db78fede44147761035333
jinalsigma <jinal.shah@sigmainfo.net>
2020-10-12 18:14:03 +0530

    Changes in the UI in all files

--- Jinal/index.js
+++ Jinal/index.js
@@ -1,8 +1,8 @@
 /**
  * Created by jinalshah on 12/10/20.
  */
-var age =parseInt(prompt('Enter your age1: '));
-alert(age);
+var age =parseInt(prompt('Enter your age: '));
+
    if(age > 60)
     {
         location.replace("earlybirds.html");
@@ -15,4 +15,7 @@ alert(age);
     {
         location.replace("foodholic.html");
     }
-
+// if(isNaN(age))
+// {
+//     location.replace("index.html");
+// }
